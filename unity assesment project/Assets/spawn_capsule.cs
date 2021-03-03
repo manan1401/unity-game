7c67d7edd7dce7b4a729b25a315dc5b6c40c6e2a
mananmk <manankotak182000@gmail.com>
2021-03-03 15:24:16 +0530

    i have tried all the tasks and completed some

---
+++ unity assesment project/Assets/spawn_capsule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_capsule : MonoBehaviour
+{
+    public Transform prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
